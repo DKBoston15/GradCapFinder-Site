ed2d45c5d16187bada30991fe6d112f08e4d9394
DKBoston15 <46634008+DKBoston15@users.noreply.github.com>
2021-05-08 13:25:28 -0700

    Initial Commit - Boilerplat - Chakra UI Instllation/Custom Theme

---
+++ src/@chakra-ui/gatsby-plugin/theme.js
@@ -0,0 +1,9 @@
+// src/@chakra-ui/gatsby-plugin/theme.js
+import { extendTheme } from "@chakra-ui/react";
+const theme = {
+    colors: {
+        primary: "red"
+    }
+};
+
+export default extendTheme(theme);
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+// src/pages/index.js
+import React from "react";
+import { Box, Text, Button } from "@chakra-ui/react";
+
+function IndexPage() {
+    return <Box>Welcome</Box>;
+}
+
+export default IndexPage;
