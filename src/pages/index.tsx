254f2bb910f615501b09cda57b24f2db032c1da9
DKBoston15 <46634008+DKBoston15@users.noreply.github.com>
2021-05-10 23:06:25 -0700

    Base About Content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Flex } from "@chakra-ui/react";
+import { Helmet } from "react-helmet";
 // Components
 import Hero from "../modules/Hero/Hero";
 import Info from "../modules/Info/Info";
 import Book from "../modules/Book/Book";
-import System from "../modules/System/System";
+import About from "../modules/About/About";
 import Videos from "../modules/Videos/Videos";
 import ContactUs from "../modules/ContactUs/ContactUs";
 import Footer from "../modules/Footer/Footer";
@@ -12,10 +13,15 @@ import Footer from "../modules/Footer/Footer";
 function IndexPage() {
     return (
         <Flex flexDirection="column" justify="space-between" w="100%">
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>GradCapFinder</title>
+                <link rel="canonical" href="http://gradcapfinder.com" />
+            </Helmet>
             <Hero />
             <Info />
             <Book />
-            <System />
+            <About />
             <Videos />
             <ContactUs />
             <Footer />
