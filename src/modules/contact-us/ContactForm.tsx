ab0d5482679b88fdbfb46bf5069661163b9d7696
DKBoston15 <46634008+DKBoston15@users.noreply.github.com>
2021-05-15 15:47:59 -0700

    EmailJS Form Setup

--- src/modules/contact-us/ContactForm.tsx
+++ src/modules/contact-us/ContactForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import {
     FormControl,
     FormLabel,
@@ -10,15 +10,69 @@ import {
     Text,
     Button
 } from "@chakra-ui/react";
+import emailjs from "emailjs-com";
 
 // Components
 import CustomSelect from "./CustomSelect";
 
 const ContactForm = () => {
-    const onSubmit = (event) => {
-        console.log(event.target);
-        event.preventDefault();
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [description, setDescription] = useState();
+    const [services, setServices] = useState([]);
+    const onChange = (e) => {
+        if (Array.isArray(e)) {
+            setServices(e);
+            return;
+        }
+        if (e.target.id == "name") {
+            setName(e.target.value);
+            return;
+        }
+        if (e.target.id == "email") {
+            setEmail(e.target.value);
+            return;
+        }
+        if (e.target.id == "description") {
+            setDescription(e.target.value);
+            return;
+        }
     };
+
+    const formReset = () => {
+        setName("");
+        setEmail("");
+        setDescription("");
+        setServices([]);
+    };
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        let templateParams = {
+            name,
+            email,
+            description,
+            services
+        };
+
+        emailjs
+            .send(
+                "service_egc5313",
+                "template_zses0cj",
+                templateParams,
+                "user_MZDgArYLRizB7rNHlh6Bn"
+            )
+            .then(
+                function (response) {
+                    console.log("SUCCESS!", response.status, response.text);
+                    formReset();
+                },
+                function (error) {
+                    console.log("FAILED...", error);
+                }
+            );
+    }
     return (
         <Flex
             justify="center"
@@ -27,65 +81,83 @@ const ContactForm = () => {
             w={{ sm: "27em", md: "35em" }}
         >
             {/* @ts-ignore */}
-            <form name="contact-us" netlify onSubmit={onSubmit}>
-                <FormControl id="name">
+            <form name="contact-us" id="contact-form" onSubmit={sendEmail}>
+                <FormControl isRequired>
                     <FormLabel>Name</FormLabel>
-                    <Input bg="white" />
+                    <Input
+                        id="name"
+                        bg="white"
+                        value={name}
+                        onChange={onChange}
+                    />
                 </FormControl>
-                <FormControl id="email">
+                <FormControl isRequired>
                     <FormLabel>Email address</FormLabel>
-                    <Input bg="white" type="email" />
+                    <Input
+                        bg="white"
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={onChange}
+                    />
                 </FormControl>
-                <FormControl id="description">
+                <FormControl isRequired>
                     <FormLabel>Description</FormLabel>
-                    <Textarea bg="white" type="email" />
+                    <Textarea
+                        bg="white"
+                        id="description"
+                        value={description}
+                        onChange={onChange}
+                    />
                 </FormControl>
                 <Text>Services</Text>
-                <FormControl id="services">
-                    <Flex justify="center">
-                        <CheckboxGroup>
-                            <Flex direction="column" m="1em">
-                                <Checkbox
-                                    size="8xl"
-                                    value="Consulting"
-                                    mb=".5em"
-                                    icon={<CustomSelect text="Consulting" />}
-                                />
-                                <Checkbox
-                                    size="8xl"
-                                    value="Writing"
-                                    icon={<CustomSelect text="Writing" />}
-                                />
-                            </Flex>
-                            <Flex direction="column" m="1em">
-                                <Checkbox
-                                    size="8xl"
-                                    value="Analysis"
-                                    mb=".5em"
-                                    icon={<CustomSelect text="Analysis" />}
-                                />
-                                <Checkbox
-                                    size="8xl"
-                                    value="Grants"
-                                    icon={<CustomSelect text="Grants" />}
-                                />
-                            </Flex>
-                            <Flex direction="column" m="1em">
-                                <Checkbox
-                                    size="8xl"
-                                    mb=".5em"
-                                    value="Graduate School"
-                                    icon={<CustomSelect text="University" />}
-                                />
-                                <Checkbox
-                                    size="8xl"
-                                    value="Other"
-                                    icon={<CustomSelect text="Other" />}
-                                />
-                            </Flex>
-                        </CheckboxGroup>
-                    </Flex>
-                </FormControl>
+                <Flex justify="center">
+                    <CheckboxGroup
+                        id="services"
+                        onChange={onChange}
+                        value={services}
+                    >
+                        <Flex direction="column" m="1em">
+                            <Checkbox
+                                size="8xl"
+                                value="Consulting"
+                                mb=".5em"
+                                icon={<CustomSelect text="Consulting" />}
+                            />
+                            <Checkbox
+                                size="8xl"
+                                value="Writing"
+                                icon={<CustomSelect text="Writing" />}
+                            />
+                        </Flex>
+                        <Flex direction="column" m="1em">
+                            <Checkbox
+                                size="8xl"
+                                value="Analysis"
+                                mb=".5em"
+                                icon={<CustomSelect text="Analysis" />}
+                            />
+                            <Checkbox
+                                size="8xl"
+                                value="Grants"
+                                icon={<CustomSelect text="Grants" />}
+                            />
+                        </Flex>
+                        <Flex direction="column" m="1em">
+                            <Checkbox
+                                size="8xl"
+                                mb=".5em"
+                                value="Graduate School"
+                                icon={<CustomSelect text="University" />}
+                            />
+                            <Checkbox
+                                size="8xl"
+                                value="Other"
+                                icon={<CustomSelect text="Other" />}
+                            />
+                        </Flex>
+                    </CheckboxGroup>
+                </Flex>
                 <Flex w={{ sm: "27em", md: "35em" }} justify="center" mt="2em">
                     <Button
                         width="100%"
