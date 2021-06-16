fb8074bd3768611489a11771034d33a858ba852d
DKBoston15 <46634008+DKBoston15@users.noreply.github.com>
2021-06-15 22:11:12 -0500

    Feature Refactor for Bolding

--- src/modules/info/Why.tsx
+++ src/modules/info/Why.tsx
@@ -16,15 +16,21 @@ const Why = () => {
             </Text>
             <Feature
                 title="Community"
-                description="We have seen that the most successful PhD students create a strong community to leverage their support and guidance in the PhD process. At GCF, we help students identify the existing members of their community and find new mentors. The world map displays where our students are located and the community of PhDs we have built at GCF."
+                description1="We have seen that the most successful PhD students create a strong community to leverage their support and guidance in the PhD process."
+                description2="At GCF, we help students identify the existing members of their community and find new mentors."
+                description3="The world map displays where our students are located and the community of PhDs we have built at GCF."
             />
             <Feature
                 title="Assessment"
-                description="We recognize that successful PhD candidates continuously assess themselves and their work. We aim to provide students constructive opportunities to evaluate themselves. This process pushes students to strive for constant improvement and learning. GCF has supported students in their growth journey all over the world."
+                description1="We recognize that successful PhD candidates continuously assess themselves and their work."
+                description2="We aim to provide students constructive opportunities to evaluate themselves."
+                description3="This process pushes students to strive for constant improvement and learning. GCF has supported students in their growth journey all over the world."
             />
             <Feature
                 title="Knowledge"
-                description="GCF supports the process of producing knowledge for the world to consume. Through our work with our PhD community we help students directly translate their efforts into knowledge. Join our global community of knowledge creators so we can support your effort to make a difference."
+                description1="GCF supports the process of producing knowledge for the world to consume."
+                description2="Through our work with our PhD community we help students directly translate their efforts into knowledge."
+                description3="Join our global community of knowledge creators so we can support your effort to make a difference."
             />
         </Flex>
     )
